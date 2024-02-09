e365e5345fa3e0ee0c4cbe59fc18df9c202f6411
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-02-09 01:03:03 +0100

    Finished setting up client

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,37 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { io } from 'socket.io-client'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const socket = io("http://127.0.0.1:3000")
+
+  socket.on("connect",()=>{
+    console.log("connection successful")
+    console.log(socket.id)
+    socket.emit("salutations","Hello World, I was written by Jules")
+
+  })
+
+  socket.on("message",(message)=>{
+    console.log(`message received: ${message}`)
+    setMessage(message)
+  })
+
+  socket.on("disconnect",()=>{
+    console.log("disconnected")
+  })
+
+  const [message,setMessage] =  useState();
 
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Message:</h2>
+        <h1>{{message}}</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    );
+  }
 
 export default App
